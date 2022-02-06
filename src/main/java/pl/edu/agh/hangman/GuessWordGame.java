108bbb292335e95ac97394e63707dd4dfe0b8c28
juzerBasia <basiawielek@gmail.com>
2022-02-06 13:56:54 +0100

    Select word

---
+++ src/main/java/pl/edu/agh/hangman/GuessWordGame.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.hangman;
+
+import java.util.Scanner;
+
+public class GuessWordGame {
+
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (count < 7) {
+			System.out.println("Guess letter");
+			String userGuess = scanner.next();
+
+		}
+
+	}
+
+
+
+}
